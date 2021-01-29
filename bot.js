1344dc0cc6e0ad0e975ec26facd36b58495b804d
SadMap <71943073+SadMap@users.noreply.github.com>
2021-01-29 09:10:27 +0200

    Some  changes

--- bot.js
+++ bot.js
@@ -3,38 +3,39 @@ const client = new Discord.Client();
 const config = require('./config.json');
 
 client.on('ready', () => {
-  console.log(`${client.user.tag} now active!`);
+	console.log(`${client.user.tag} now active!`);
 });
 
-client.on('messageReactionAdd', async (reaction, user) => {
+client.on('messageReactionAdd', async (reaction) => {
 
-  if (reaction.message.channel.id !== config.channel) {
-    if (reaction.emoji.name === config.emoji) {
-      if (reaction.count === 3) {
-        if (reaction.message.attachments.array().length > 0) {
-          var image = reaction.message.attachments.array()[0].url
-        }
-        var content = reaction.message.content
-        var starChannel = reaction.message.guild.channels.cache.get(config.channel)
+	if (reaction.message.channel.id !== config.channel) {
+		if (reaction.emoji.name === config.emoji) {
+			if (reaction.count === config.starcount) {
+				let image = undefined;
+				if (reaction.message.attachments.array().length > 0) {
+					image = reaction.message.attachments.array()[0].url;
+				}
+				const content = reaction.message.content;
+				const starChannel = reaction.message.guild.channels.cache.get(config.channel);
 
-        var link = `https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}`
-        const embed = new Discord.MessageEmbed()
+				const link = reaction.message.url;
+				const embed = new Discord.MessageEmbed()
 
-          .setAuthor(reaction.message.author.username, reaction.message.author.avatarURL)
+					.setAuthor(reaction.message.author.username, reaction.message.author.avatarURL)
 
-          .addField('Jump to Message', `[Click here](${link})`)
+					.addField('Jump to Message', `[Click here](${link})`);
 
-        if (content !== '') {
-          embed.setDescription(reaction.message.content)
-        }
+				if (content !== '') {
+					embed.setDescription(reaction.message.content);
+				}
 
-        if (image !== undefined) {
-          embed.setImage(image)
-        }
-        starChannel.send(embed)
-      }
-    }
-  }
+				if (image !== undefined) {
+					embed.setImage(image);
+				}
+				starChannel.send(embed);
+			}
+		}
+	}
 });
 
 client.login(config.token);
